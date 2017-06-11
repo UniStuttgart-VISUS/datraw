b84097882642dc8c31e8bc10f6d1a98185e7e0b3
crowbar27 <christophmueller@web.de>
2017-06-11 15:27:42 +0200

    Added info.cpp

---
+++ datraw/info.cpp
@@ -0,0 +1,11 @@
+/// <copyright file="info.cpp" company="Visualisierungsinstitut der Universität Stuttgart">
+/// Copyright © 2017 Visualisierungsinstitut der Universität Stuttgart. Alle Rechte vorbehalten.
+/// </copyright>
+/// <author>Christoph Müller</author>
+
+
+#include "datraw/info.h"
+
+#include <array>
+
+//static const std::array<std::
