3097155001d8dd2b3fed3c98f72aafb80f3d20cf
Christoph <christoph.mueller@visus.uni-stuttgart.de>
2017-06-12 17:24:13 +0200

    Fixed warning.

--- t/t.cpp
+++ t/t.cpp
@@ -7,6 +7,7 @@
 
 int main() {
     auto info = datraw::info<char>::parse("S:\\Daten\\Volumen\\tuebingen\\foot.dat");
+    auto winfo = datraw::info<wchar_t>::parse(L"S:\\Daten\\Volumen\\tuebingen\\foot.dat");
 
     return 0;
 }
